421494f25f6b7fa3d2b2686bccec2c62f9dddbbb
Shagun Madhikarmi <madhikarma@gmail.com>
2020-07-18 02:07:32 +0100

    Adds some comments

--- StoryboardDemo/ViewController.swift
+++ StoryboardDemo/ViewController.swift
@@ -11,6 +11,7 @@ import CoreData
 
 final class ViewController: UIViewController {
 
+
     // MARK: - Properties
     
     private var applicationDelegate: AppDelegate {
@@ -23,39 +24,58 @@ final class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchStoryboards()
+    }
+    
+    private func fetchStoryboards() {
         
         // Load all Storyboard entities from CoreData database
+        
+        // Create a fetch requset for a Storyboard
         let fetchRequest = NSFetchRequest<Storyboard>(entityName: "Storyboard")
+        
+        // Get the Core Data context that represents the database (TODO: optimsie with background context)
         let context = applicationDelegate.persistentContainer.viewContext
-        var objects: [Storyboard] = []
+        
+        // Fetch the storyboards from the context
+        var storyboards: [Storyboard] = []
         do {
-            objects = try context.fetch(fetchRequest)
+            storyboards = try context.fetch(fetchRequest)
         } catch {
             print("Error: error fetching: \(error)")
         }
         // TODO: display in UI
-        print(objects)
-        objects.forEach { print($0.name) }
+        print(storyboards)
+        storyboards.forEach { print(String(describing: $0.name)) }
     }
     
     
     // MARK: - Actions
     
+    
     @IBAction func addButtonPressed(_ sender: Any) {
+        
+        // Get the Core Data context that represents the database (TODO: optimsie with background context)
         let context = applicationDelegate.persistentContainer.viewContext
         
         // Create new Storyboard entity
         let storyboard: Storyboard = NSEntityDescription.insertNewObject(forEntityName: "Storyboard",
                                                                          into: context) as! Storyboard
-        // assign its properties
-        storyboard.id = UUID()
-        storyboard.name = "Test"
+        
         
         // TODO: create Frame entities with image paths and add those to the new Storyboard
         
+        storyboard.id = UUID()
+        storyboard.name = "Test"
         
         // Save new entities into the core data database
         applicationDelegate.saveContext()
+        
+        // Show UI alert popup for debug
+        let alert = UIAlertController(title: "Success", message: "Created Storyboard: \(storyboard)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
     }
 }
 
