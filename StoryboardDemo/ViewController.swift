853d5880022d74d1f3118ca56ccebcbf370841b0
Shagun Madhikarmi <madhikarma@gmail.com>
2020-07-18 02:00:15 +0100

    Adds code to ready all Storyboards on load and add new one into the CoreData database

--- StoryboardDemo/ViewController.swift
+++ StoryboardDemo/ViewController.swift
@@ -7,14 +7,55 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    private var applicationDelegate: AppDelegate {
+        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate) else { fatalError() }
+        return appDelegate
     }
 
-
+    
+    // MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load all Storyboard entities from CoreData database
+        let fetchRequest = NSFetchRequest<Storyboard>(entityName: "Storyboard")
+        let context = applicationDelegate.persistentContainer.viewContext
+        var objects: [Storyboard] = []
+        do {
+            objects = try context.fetch(fetchRequest)
+        } catch {
+            print("Error: error fetching: \(error)")
+        }
+        // TODO: display in UI
+        print(objects)
+        objects.forEach { print($0.name) }
+    }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let context = applicationDelegate.persistentContainer.viewContext
+        
+        // Create new Storyboard entity
+        let storyboard: Storyboard = NSEntityDescription.insertNewObject(forEntityName: "Storyboard",
+                                                                         into: context) as! Storyboard
+        // assign its properties
+        storyboard.id = UUID()
+        storyboard.name = "Test"
+        
+        // TODO: create Frame entities with image paths and add those to the new Storyboard
+        
+        
+        // Save new entities into the core data database
+        applicationDelegate.saveContext()
+    }
 }
 
